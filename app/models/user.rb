a1c66dc9e5b38668f9345f7e332eba399de174a4
Marsha <gospodarek@gmail.com>
2010-12-02 18:21:19 +0100

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
                     :length   => { :maximum => 50 }
                     
   validates :email, :presence => true,
-                    :format   => { :with => email_regex }
-                    
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }                    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Travelsome::Application.routes.draw do
-  get "users/new"
+  resources :users
   
   match '/signup',  :to => 'users#new'
 
